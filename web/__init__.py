3b41c95d9f061767671c1b613b12c9c9a3ac5806
Ahmad Sattar Atta <asa@delegate.dk>
2020-09-17 16:27:17 +0200

    Blueprint and openapi

--- web/__init__.py
+++ web/__init__.py
@@ -2,15 +2,26 @@
 from __future__ import absolute_import, print_function
 import os
 from flask import Flask
-from dash import Dash
 from flask_assets import Environment
 from flask_login import LoginManager
 from .models.users_management import db, User, Role
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from apispec_webframeworks.flask import FlaskPlugin
+from .routes import myblueprint
+
+
+spec = APISpec(
+    title="Test",
+    version="1.0.0",
+    openapi_version="3.0.2",
+    plugins=[FlaskPlugin(), MarshmallowPlugin()],
+)
+
 
 def script_runs_within_container():
     with open("/proc/1/cgroup", "r") as cgroup_file:
         return "docker" in cgroup_file.read()
-
 
 def create_app():
     """Construct core Flask application with embedded Dash app."""
@@ -21,12 +32,7 @@ def create_app():
 
     with server.app_context():
         # Import parts of our core Flask app
-        from . import routes
         from .assets import compile_static_assets
-
-        # Import Dash application
-        # from .plotlydash.dashboard import create_dashboard
-        # app = create_dashboard(app)
 
         server.config.update(
             SECRET_KEY=os.urandom(12),
@@ -48,18 +54,21 @@ def create_app():
         def load_user(user_id):
             return User.objects(pk=user_id).first()
 
-        app = Dash(
-            server=server,
-            routes_pathname_prefix="/",
-            external_stylesheets=["/static/dist/css/flatly-custom.css"],
-        )
-
-        from web.plotlydash.index import layout, init_callbacks
-
-        app.layout = layout
-        init_callbacks(app)
-
         # Compile static assets
         # compile_static_assets(assets)
+
+        server.register_blueprint(myblueprint)
+
+        @server.route("/a")
+        def swagger2():
+            return "as"
+
+        with server.test_request_context():
+            for rule in server.url_map.iter_rules():
+                spec.path(view=server.view_functions[rule.endpoint])
+
+        @server.route("/swagger.json")
+        def swagger():
+            return json.dumps(spec.to_dict(), indent=2)
 
         return server
--- web/routes.py
+++ web/routes.py
@@ -1,15 +1,30 @@
 """Routes for parent Flask app."""
 from flask import render_template
 from flask import current_app as app
+import json
+from flask import Blueprint
 
 
-@app.route("/test_flask")
-def home():
-    """Landing page."""
-    return render_template(
-        "index.jinja2",
-        title="Startside",
-        description="Dash embedded into flask",
-        template="home-template",
-        body="This is a homepage served with Flask.",
-    )
+myblueprint = Blueprint('routes', __name__)
+
+@myblueprint.route("/random")
+def random_pet():
+    """A cute furry animal endpoint.
+    ---
+    get:
+    description: Get a random pet
+    security:
+        - ApiKeyAuth: []
+    responses:
+        200:
+        description: Return a pet
+        content:
+            application/json:
+            schema: PetSchema
+    """
+    # Hardcoded example data
+    pet_data = {
+        "name": "sample_pet_" + str(uuid.uuid1()),
+        "categories": [{"id": 1, "name": "sample_category"}],
+    }
+    return PetSchema().dump(pet_data)
