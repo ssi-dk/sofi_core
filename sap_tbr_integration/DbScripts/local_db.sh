4b9fe0d6330b4d5d8e7410d101f3326e73aab7ad
Ahmad Sattar Atta <asa@delegate.dk>
2021-01-13 13:43:34 +0100

    Local TBR from script

--- sap_tbr_integration/DbScripts/local_db.sh
+++ sap_tbr_integration/DbScripts/local_db.sh
@@ -1,1 +1,7 @@
-docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=MyStrongPassword123' -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest
+docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=MyStrongPassword123' --mount src="$(pwd)",target=/scripts,type=bind --network sap_default --name sdb -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest
+sleep 30
+docker exec -it sdb /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'MyStrongPassword123' -i /scripts/create_db.sql
+docker exec -it sdb /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'MyStrongPassword123' -i /scripts/alter_db.sql
+docker exec -it sdb /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'MyStrongPassword123' -i /scripts/dummy_data.sql
+docker exec -it sdb /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'MyStrongPassword123' -i /scripts/get_isolate_sp.sql
+docker exec -it sdb /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'MyStrongPassword123' -i /scripts/update_isolate_sp.sql
