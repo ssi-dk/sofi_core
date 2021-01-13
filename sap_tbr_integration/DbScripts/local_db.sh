e0cf8db0b31c13ba4c8407da865bebc6b1cddf07
Ahmad Sattar Atta <asa@delegate.dk>
2021-01-13 14:41:04 +0100

    readonly sql scripts

--- sap_tbr_integration/DbScripts/local_db.sh
+++ sap_tbr_integration/DbScripts/local_db.sh
@@ -1,4 +1,4 @@
-docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=MyStrongPassword123' --mount src="$(pwd)",target=/scripts,type=bind --network sap_default --name sdb -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest
+docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=MyStrongPassword123' --mount src="$(pwd)",target=/scripts,type=bind,readonly --network sap_default --name sdb -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest
 sleep 30
 docker exec -it sdb /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'MyStrongPassword123' -i /scripts/create_db.sql
 docker exec -it sdb /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'MyStrongPassword123' -i /scripts/alter_db.sql
