06c7ede73fcfc01ff0bc6b11ce34d44b0ecd25ca
Ahmad Sattar Atta <asa@delegate.dk>
2021-01-29 16:15:25 +0100

    New encryption based mongoclient

--- web/src/SAP/__init__.py
+++ web/src/SAP/__init__.py
@@ -1,9 +1,15 @@
 import connexion
 from .generated import encoder
+from .src.repositories.database import get_connection
+
 
 
 def create_app():
     app = connexion.App(__name__, specification_dir="../../../openapi_specs/SAP/")
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api("SAP.yaml", arguments={"title": "SAP"}, pythonic_params=True)
+    
+    ## Start the encrypted connection instance on app boot
+    get_connection()
+    
     return app.app
