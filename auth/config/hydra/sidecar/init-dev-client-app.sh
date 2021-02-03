4e39a55794458a95d457ee756007484ce6d3dd8b
Brandon Lucas (Delegate) <blu@delegate.dk>
2021-02-03 09:54:06 +0100

    Support TLS with termination past the proxy
    This commit makes the proxy addressable by the other services,
    so the individual services can communicate with one another via
    TLS by addressing each other through the proxy.
    
    There are some services (kratos Admin API, hydra Admin API) which
    are not exposed via the proxy. Communication with these is still
    direct and via HTTP. If needed, we could allow routing to these
    via the proxy too, and apply some CIDR-based filtering to prevent
    exposing them outside the docker network. Or introduce an
    intermediate proxy.
    
    But for now, the intention is that this communication with the
    Admin APIs will always be on the same VM, so TLS is not vital.
    
    Note: Services expect a verifiable certificate. Self-signed certs
    do not work yet.

--- auth/config/hydra/sidecar/init-dev-client-app.sh
+++ auth/config/hydra/sidecar/init-dev-client-app.sh
@@ -1,13 +1,14 @@
 #/usr/bin/env sh
 
-set -e
+set -euo pipefail
 
 # Ensure the public client application is registered when running locally
-echo "Generating client"
-hydra clients create --endpoint 'http://127.0.0.1:4445' \
+
+echo "Ensuring client application registered ..."
+
+hydra clients create --endpoint 'hydra:4445' \
     --id ${SOFI_CLIENT_ID} \
     --token-endpoint-auth-method none \
     --scope openid,offline,offline_access,email,profile \
     --grant-types authorization_code,refresh_token \
-    --callbacks "https://${SOFI_HOSTNAME}:${SOFI_PORT}/callback" \
-    --fake-tls-termination
+    --callbacks "https://${SOFI_HOSTNAME}:${SOFI_PORT}/callback"
