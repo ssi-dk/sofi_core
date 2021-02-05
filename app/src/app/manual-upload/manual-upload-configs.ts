617346c0025daa3f090039a2d375dd97a637b97b
Ahmad Sattar Atta <asa@delegate.dk>
2021-02-05 12:48:47 +0100

    Tabbed upload page

--- app/src/app/manual-upload/manual-upload-configs.ts
+++ app/src/app/manual-upload/manual-upload-configs.ts
@@ -1,11 +1,8 @@
-import {
-  IsolateUploadRequest,
-  isolateUpload
-} from "sap-client";
+import { IsolateUploadRequest, isolateUpload } from "sap-client";
 import { getUrl } from "service";
 
 export const uploadIsolateFile = (req: IsolateUploadRequest) => {
   const base = isolateUpload<void>(req);
   base.url = getUrl(base.url);
   return base;
-}
+};
--- app/src/app/manual-upload/manual-upload-page.tsx
+++ app/src/app/manual-upload/manual-upload-page.tsx
@@ -1,23 +1,54 @@
 import React, { useState } from "react";
-import { Spacer, Box, Flex, Heading } from "@chakra-ui/react"
-import UploadForm from "./upload-form"
+
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Spacer,
+  Box,
+  Flex,
+  Heading,
+} from "@chakra-ui/react";
+import SingleUploadForm from "./single-upload-form";
+import MultiUploadForm from "./multi-upload-form";
+import BulkUploadForm from "./bulk-upload-form";
+import Header from "../header/header";
 
 export default function ManualUploadPage() {
   return (
-    <Flex
-      direction="column"
-      align="center"
-      maxW={{ xl: "1200px" }}
-      m="0 auto"
-
+    <Box
+      display="grid"
+      gridTemplateRows="10% auto"
+      gridTemplateColumns="18rem auto 18rem"
+      padding="8"
+      height="100vh"
+      gridGap="5"
     >
-      <Box minW="100%" flexShrink={0}>
-        <Heading>SOFI</Heading>
+      <Box role="heading" gridColumn="1 / 3">
+        <Header sidebarWidth="300px" />
       </Box>
-      
-      <Spacer />
-
-      <UploadForm />
-    </Flex>
+      <Box gridColumn="2 / 2">
+        <Tabs variant="soft-rounded">
+          <TabList>
+            <Tab _selected={{ bg: "blue.100" }}>Single upload</Tab>
+            <Tab _selected={{ bg: "blue.100" }}>Multi upload</Tab>
+            <Tab _selected={{ bg: "blue.100" }}>Bulk metadata upload</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <SingleUploadForm />
+            </TabPanel>
+            <TabPanel>
+              <MultiUploadForm />
+            </TabPanel>
+            <TabPanel>
+              <BulkUploadForm />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Box>
   );
 }
