7f20411500997cc72a00a605ff375f7e876e7112
Brandon Lucas (Delegate) <blu@delegate.dk>
2021-02-10 22:15:37 +0100

    Fix inputs losing focus due to re-render on single-upload-form

---
+++ app/src/app/manual-upload/text-input.tsx
@@ -0,0 +1,31 @@
+import { FormControl, FormLabel, Input } from "@chakra-ui/react";
+import React from "react";
+
+type TextInputProps = {
+  label: string;
+  name: string;
+  value: string | number | Date | boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const TextInput = (props: TextInputProps) => {
+  const { name, value, label, onChange } = props;
+  if (name.endsWith("date")) {
+    return (
+      <>
+        <FormControl id={name}>
+          <FormLabel>{label}</FormLabel>
+          <Input type="date" value={value as string} name={name} onChange={onChange} />
+        </FormControl>
+      </>
+    );
+  }
+  return (
+    <FormControl id={name}>
+      <FormLabel>{label}</FormLabel>
+      <Input type="text" value={value as string} name={name} onChange={onChange} />
+    </FormControl>
+  );
+};
+
+export default React.memo(TextInput);
