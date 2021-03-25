f7dbd0fca17b1101ef72ac8801a445a448dc4ec4
Ahmad Sattar Atta <asa@delegate.dk>
2021-03-25 16:45:14 +0100

    Centering

--- app/src/app/comparative-analysis/sidebar/node-clustering-filter/node-coloring-filter.tsx
+++ app/src/app/comparative-analysis/sidebar/node-clustering-filter/node-coloring-filter.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Box, Flex, Heading } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 import FilterBox from "../filter-box";
-import IndicatorIcon from "../indicator-icon"
+import IndicatorIcon from "../indicator-icon";
 
 export default function NodeColoringFilter() {
   const { t } = useTranslation();
@@ -11,7 +11,10 @@ export default function NodeColoringFilter() {
     <FilterBox title={t("Node coloring")}>
       <Flex direction="column" overflowY="scroll" maxHeight="12rem">
         {[...Array(25)].map((_, i) => (
-          <div key={Math.round(i)}><IndicatorIcon color="blue.500" />{i}</div>
+          <Flex key={Math.round(i)} alignItems="center">
+            <IndicatorIcon color="blue.500" />
+            {i}
+          </Flex>
         ))}
       </Flex>
     </FilterBox>
