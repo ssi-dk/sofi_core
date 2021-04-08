0f0fd8f99ab1042656a1f9dffe17a42bc033668d
Ahmad Sattar Atta <asa@delegate.dk>
2021-04-08 13:53:45 +0200

    Update mappings for LIMS

--- bifrost/bifrost_queue_broker/brokers/lims_conn.py
+++ bifrost/bifrost_queue_broker/brokers/lims_conn.py
@@ -8,6 +8,8 @@ from api_clients.lims_client.models import (
     IsolateGetResponse,
 )
 
+from .shared import column_mapping
+
 lims_api_url = os.environ.get("LIMS_API_URL")
 lims_api_databaseid = os.environ.get("LIMS_API_DATABASEID")
 lims_api_username = os.environ.get("LIMS_API_USERNAME")
@@ -15,10 +17,21 @@ lims_api_password = os.environ.get("LIMS_API_PASSWORD")
 
 lims_configuration = api_clients.lims_client.Configuration(host=lims_api_url)
 
+
 def transform_lims_metadata(lims_metadata: IsolateGetResponse):
-    metadata = {md.meta_field_name.value:md.meta_field_value for md in lims_metadata.output.sapresponse.metadata}
-    data = {d.field_name.value:d.field_value for d in lims_metadata.output.sapresponse.data}
-    return {"isolate_id": lims_metadata.output.sapresponse.isolate_id,**metadata, **data}
+    metadata = {
+        column_mapping[md.meta_field_name.value]: md.meta_field_value
+        for md in lims_metadata.output.sapresponse.metadata
+    }
+    data = {
+        column_mapping[d.field_name.value]: d.field_value
+        for d in lims_metadata.output.sapresponse.data
+    }
+    return {
+        "isolate_id": lims_metadata.output.sapresponse.isolate_id,
+        **metadata,
+        **data,
+    }
 
 
 def create_lims_conn_config():
@@ -33,20 +46,22 @@ def create_lims_conn_config():
         )
         connection_id = None
         try:
-            api_response: ConnectionCreateResponse = conn_create_instance.post_connections(
-                connection_create_request=conn_req
+            api_response: ConnectionCreateResponse = (
+                conn_create_instance.post_connections(
+                    connection_create_request=conn_req
+                )
             )
             connection_id = api_response.connections.connectionid
         except api_clients.lims_client.ApiException as e:
             print("Exception when creating connection: %s\n" % e)
-    
+
     lms_cfg = api_clients.lims_client.Configuration(
-        host=lims_api_url,
-        api_key={'cookieAuth': f"connectionid={connection_id}"}
+        host=lims_api_url, api_key={"cookieAuth": f"connectionid={connection_id}"}
     )
 
     return connection_id, lms_cfg
-    
+
+
 def close_lims_connection(connection_id, lms_cfg):
     with api_clients.lims_client.ApiClient(lms_cfg) as api_client:
         conn_delete_instance = connections_api.ConnectionsApi(api_client)
--- bifrost/bifrost_queue_broker/brokers/shared.py
+++ bifrost/bifrost_queue_broker/brokers/shared.py
@@ -7,3 +7,82 @@ class BrokerError(Exception):
     """Thrown when a broker encounters an error."""
 
     pass
+
+
+class immutable_dict(dict):
+    def __init__(self, *args, **kwargs):
+        super(immutable_dict, self).__init__(*args, **kwargs)
+
+    def __getitem__(self, key):
+        # val = dict.__getitem__(self, key)
+        val = self.get(key, key)
+        return val
+
+    def __setitem__(self, key, val):
+        pass
+
+
+column_mapping = immutable_dict(
+    {
+        # LIMS mapping
+        "isolateId": "isolate_id",
+        "SekvensId": "sequence_id",
+        "Sekvensfilnavn": "sequence_filename",
+        "Projektnr": "project_number",
+        "Projekttitel": "project_title",
+        # LIMS api returns a space after Dato_proeve currently.
+        "Dato_proeve ": "sampling_date",
+        "Dato_modtagelse": "received_date",
+        "RunID": "run_id",
+        "Acc_nr": "public",
+        "QC_provided_species": "provided_species",
+        "Primaert_iso": "primary_isolate",
+        "Dato_modtagelse": "received_date",
+        "CHR": "chr_number",
+        "CVR": "cvr_number",
+        "Aut_nr": "aut_number",
+        "Produkt_type": "product_type",
+        "Produkt": "product",
+        "Oprindelsesland": "origin_country",
+        "Dyreart": "animal_species",
+        "Proeveinfo": "sample_info",
+        "AMR_profil": "amr_profil",
+        "AMR _ Ami": "amr_ami",
+        "AMR _ Amp": "amr_amp",
+        "AMR _ Azi": "amr_azi",
+        "AMR _ Fep": "amr_fep",
+        "AMR _ Fot": "amr_fot",
+        "AMR _ F/C": "amr_f/c",
+        "AMR _ Fox": "amr_fox",
+        "AMR _ Taz": "amr_taz",
+        "AMR _ T/C": "amr_t/c",
+        "AMR _ Chl": "amr_chl",
+        "AMR _ Cip": "amr_cip",
+        "AMR _ Cli": "amr_cli",
+        "AMR _ Col": "amr_col",
+        "AMR _ Dap": "amr_dap",
+        "AMR _ Etp": "amr_etp",
+        "AMR _ Ery": "amr_ery",
+        "AMR _ Fus": "amr_fus",
+        "AMR _ Gen": "amr_gen",
+        "AMR _ Imi": "amr_imi",
+        "AMR _ Kan": "amr_kan",
+        "AMR _ Lzd": "amr_lzd",
+        "AMR _ Mero": "amr_mero",
+        "AMR _ Mup": "amr_mup",
+        "AMR _ Nal": "amr_nal",
+        "AMR _ Pen": "amr_pen",
+        "AMR _ Syn": "amr_syn",
+        "AMR _ Rif": "amr_rif",
+        "AMR _ Str": "amr_str",
+        "AMR _ Sul": "amr_sul",
+        "AMR _ Tei": "amr_tei",
+        "AMR _ Trm": "amr_trm",
+        "AMR _ Tet": "amr_tet",
+        "AMR _ Tia": "amr_tia",
+        "AMR _ Tgc": "amr_tgc",
+        "AMR _Tmp ": "amr_tmp",
+        "AMR _ Van": "amr_van",
+        # TBR AMR mappings
+    }
+)
