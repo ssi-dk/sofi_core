d401945437e5a7829cdeb1834bbc76d9e4f7fb4e
Ahmad Sattar Atta <asa@delegate.dk>
2021-04-26 10:30:59 +0200

    New bifrost archive

--- bifrost/bifrost_db/initdb.d/10_restore_dev_db.sh
+++ bifrost/bifrost_db/initdb.d/10_restore_dev_db.sh
@@ -3,4 +3,4 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-mongorestore --archive=${DIR}/bifrost_dev_nofiles.archive --batchSize=50
+gunzip -c ${DIR}/bifrost_test.archive.gz | mongorestore --archive
