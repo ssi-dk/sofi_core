b327c08fab05f84ff948f232d546841449fbfd1e
Ahmad Sattar Atta <asa@delegate.dk>
2021-05-06 11:16:41 +0200

    Check in historic tables and triggers

--- sap_tbr_integration/DbScripts/local_db.sh
+++ sap_tbr_integration/DbScripts/local_db.sh
@@ -7,6 +7,7 @@ docker run --rm -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=MyStrongPassword123" --mount 
 sleep 50
 docker exec -it sdb /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "MyStrongPassword123" -i /scripts/create_db.sql
 docker exec -it sdb /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "MyStrongPassword123" -i /scripts/alter_db.sql
+docker exec -it sdb /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "MyStrongPassword123" -i /scripts/historic_tables.sql
 docker exec -it sdb /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "MyStrongPassword123" -i /scripts/dummy_data.sql
 docker exec -it sdb /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "MyStrongPassword123" -i /scripts/get_isolate_sps.sql
 docker exec -it sdb /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "MyStrongPassword123" -i /scripts/update_isolate_sp.sql
