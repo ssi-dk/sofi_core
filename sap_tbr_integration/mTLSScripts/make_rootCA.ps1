711cbfe05bbe31bfebe12a0408073eab8277a41e
Ahmad Sattar Atta <asa@delegate.dk>
2021-06-07 14:05:41 +0200

    Parameterize mtls scripts

--- sap_tbr_integration/mTLSScripts/make_rootCA.ps1
+++ sap_tbr_integration/mTLSScripts/make_rootCA.ps1
@@ -1,3 +1,11 @@
+param (
+    [Parameter(HelpMessage = "Hostname to give the csr (CN value)")] 
+    [string] $hostname = "sofi_tbr_integration",
+
+    [Parameter(HelpMessage = "Password for the root PFX", Mandatory = $true)] 
+    [string] $password
+)
+
 $ca_dir = "$PSScriptRoot/root"
 New-Item -ItemType Directory -Force -Path $ca_dir
 
@@ -7,13 +15,13 @@ $ca_crt = "$ca_dir/SOFI_ROOT.crt"
 $ca_pfx = "$ca_dir/SOFI_ROOT.pfx"
 
 # Generate a key
-openssl genrsa -aes256 -passout pass:test -out $ca_key 2048
+openssl genrsa -aes256 -passout pass:$password -out $ca_key 2048
 
 # Generate root certificate
-openssl req -x509 -passin pass:test -new -nodes -key $ca_key -sha256 -days 10240 -out $ca_pem -subj "/C=DK/ST=Copenhagen/L=Copenhagen/O=Delegate/CN=sofi_tbr_integration_dev"
+openssl req -x509 -passin pass:$password -new -nodes -key $ca_key -sha256 -days 10240 -out $ca_pem -subj "/C=DK/ST=Copenhagen/L=Copenhagen/O=Delegate/CN=$hostname"
 
 # Create a .crt file so it can be installed on yucky windows (can *probably* just out in this format from the step above, but i don't know much about openssl)
 openssl x509 -outform der -in $ca_pem -out $ca_crt
 
 # Generate PFX used for IIS
-openssl pkcs12 -passin pass:test -passout pass:test -inkey $ca_key -in $ca_pem -export -out $ca_pfx
+openssl pkcs12 -passin pass:$password-passout pass:$password-inkey $ca_key -in $ca_pem -export -out $ca_pfx
