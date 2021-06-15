e9f5a38a380b27bc657b4479849ba4fc65a4db7a
Ahmad Sattar Atta <asa@delegate.dk>
2021-06-15 14:41:55 +0200

    Test script for tbr

---
+++ management_scripts/test_tbr.sh
@@ -0,0 +1,23 @@
+#/usr/bin/env bash
+
+set -Eeuo pipefail
+
+# Test connectivity to TBR
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+source $SCRIPT_DIR/../.env
+
+output=$( curl -i \
+               --key $TBR_CLIENT_KEY \
+               --cert $TBR_CLIENT_CERT \
+               "$TBR_API_URL/" | grep 'HTTP/1.1')
+
+echo "$output"
+
+if [[ $output == *'403 Forbidden'* ]]; then
+    echo "TBR Connectivity: Failed"
+    exit 1
+else
+    echo "TBR Connectivity: OK"
+fi
