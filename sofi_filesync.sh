1a57c66b7f79d3954173e9ad90e9c6dbb66b64a1
Brandon Lucas (Delegate) <blu@delegate.dk>
2021-06-16 15:40:21 +0200

    Use secondary service to copy files

---
+++ sofi_filesync.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# Transfer files from /opt/sofi/upload_dir to the nfs mount in the live environment
+# This works around the inability to bind the nfs mount to a volume within the api container itself
+
+set -Eeuo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source $DIR/.env 
+
+inotifywait -mrq -e modify,create $SOFI_UPLOAD_DIR | while read DIRECTORY EVENT FILE
+do
+   mv $SOFI_UPLOAD_DIR/* $BIFROST_UPLOAD_DIR
+done
