24e4baafc70f0f6d73f72194bba0f579c3ee073d
Brandon Lucas (Delegate) <blu@delegate.dk>
2021-06-17 17:05:32 +0200

    Fix filesync script

--- sofi_filesync.sh
+++ sofi_filesync.sh
@@ -4,7 +4,7 @@
 # Transfer files from /opt/sofi/upload_dir to the nfs mount in the live environment
 # This works around the inability to bind the nfs mount to a volume within the api container itself
 
-set -Eeuo pipefail
+#set -Eeuo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -12,5 +12,8 @@ source $DIR/.env
 
 inotifywait -mrq -e modify,create $SOFI_UPLOAD_DIR | while read DIRECTORY EVENT FILE
 do
-   mv $SOFI_UPLOAD_DIR/* $BIFROST_UPLOAD_DIR/
+   filepath=${DIRECTORY#"$SOFI_UPLOAD_DIR"}${FILE}
+   destpath="$BIFROST_UPLOAD_DIR$filepath"
+   mkdir --parents "$destpath"
+   mv "$DIRECTORY/$FILE" "$destpath/"
 done
