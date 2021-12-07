9ca226699ad1d7b05c17054487bc5802ad349534
Brandon Lucas <br@ndon.dk>
2021-12-07 18:19:21 +0100

    bifrost mongodb does not yet support isNumber

--- bifrost/bifrost_listener/aggregation.py
+++ bifrost/bifrost_listener/aggregation.py
@@ -222,9 +222,9 @@ def agg_pipeline(changed_ids=None):
             "$set": {
                 "st_final": {
                     "$cond": {
-                        "if": {"$isNumber": "$st"},
-                        "then": "$st",
-                        "else": None,
+                        "if": {"$regexMatch": {"input": "$st", "regex": "\\*"}},
+                        "then": None,
+                        "else": "$st",
                     },
                 }
             }
