675751f6174deea10d50355672e38b2536773e74
Brandon Lucas <br@ndon.dk>
2022-02-11 15:23:57 +0100

    Implicitly fuzz date searches to discard time portion

--- app/src/app/analysis/search/analysis-search.tsx
+++ app/src/app/analysis/search/analysis-search.tsx
@@ -22,9 +22,17 @@ type AnalysisSearchProps = {
 const parseQuery = (input: string, toast) => {
   try {
     const ast = luceneParse(input);
-    // translate display names to internal names
     recurseTree(ast, (x) => {
+      console.log(x);
       if (x["field"]) {
+        // date fields might be represented as datetimes in the db, so do a
+        // wildcard to discard the time component
+        if (x["field"].startsWith("date_")) {
+          if (!x["term"].contains("*")) {
+            x["term"] = `${x["term"]}*`;
+          }
+        }
+        // translate display names to internal names
         x["field"] = getFieldInternalName(x["field"]) ?? x["field"];
       }
     });
