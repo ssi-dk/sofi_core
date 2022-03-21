dab4ca53a79157581e870dab36c21f2b2e59179f
Thyge S. Steffensen <tst@delegate.dk>
2022-03-21 17:13:54 +0100

    MongoDB working with replica sets - removed comments

--- bifrost/bifrost_db/initdb.d/10_restore_dev_db.sh
+++ bifrost/bifrost_db/initdb.d/10_restore_dev_db.sh
@@ -4,11 +4,3 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 #gunzip -c ${DIR}/bifrost_test.archive.gz | mongorestore --archive
-
-#mongo --eval 'use bifrost_test'
-
-# Create Admin database
-  # Create the user that will connect to it
-  # Maybe auto by mongo - create admin
-  # Check the 'old' version
-# Create `bifrost_test` database
