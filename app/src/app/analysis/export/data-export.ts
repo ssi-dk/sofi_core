df6d3c2b80de2429fd5563f10bd14ac26c45dd1f
Thyge S. Steffensen <tst@delegate.dk>
2023-03-24 13:01:03 +0100

    fix: .toISOString on date RangeError
    This is because a Date object can be ill, and thus cannot create the ISO string
    
    #104872

--- app/src/app/analysis/export/data-export.ts
+++ app/src/app/analysis/export/data-export.ts
@@ -5,8 +5,15 @@ const transform = (value: any, key: string | number | symbol) => {
     return "";
   }
   if (value instanceof Date) {
-    // Fancy libraries could be used, but this will do the trick just fine
-    return value.toISOString().split("T")[0];
+    try {
+      // Fancy libraries could be used, but this will do the trick just fine
+      return value.toISOString().split("T")[0];
+    } catch (e) {
+      if (e instanceof RangeError) {
+        // Date can be ill. E.g., `Date(undefined)` is allowed, but ill.
+        return value;
+      }
+    }
   }
 
   if (key === "qc_failed_tests") {
