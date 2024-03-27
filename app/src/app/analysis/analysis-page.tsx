b2a180c7193746ed7b0ff0d568e45a0ff258a739
Jacob Svoldgaard <jsv@delegate.dk>
2024-03-27 17:23:57 +0100

    Re-did toast message; kept more detailed info in network response.

--- app/src/app/analysis/analysis-page.tsx
+++ app/src/app/analysis/analysis-page.tsx
@@ -190,10 +190,10 @@ export default function AnalysisPage() {
     if (health) {
       if (health.hasOwnProperty("lims") && health.hasOwnProperty("tbr")) {
         if (health["tbr"] && health["tbr"].status == HealthStatus.Unhealthy) {
-          messages.push(health["tbr"].description);
+          messages.push("Could not connect to TBR.");
         }
         if (health["lims"] && health["lims"].status == HealthStatus.Unhealthy) {
-          messages.push(health["lims"].description);
+          messages.push("Could not connect to LIMS.");
         }
       }
 
