afb50ff39342dc730ab172e5c2c5d00a0184702d
Simon J.K. Pedersen (Delegate) <skp@delegate.dk>
2024-06-05 13:21:57 +0200

    chore!: remove hydra/kratos references.

--- auth/config/hydra/healthcheck.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh 
-
-set -e
-
-FIRST_READY_STATUS_FLAG='/tmp/.FIRST_READY_STATUS_FLAG'
-
-if [ ! -f "${FIRST_READY_STATUS_FLAG}" ]; then
-    hydra clients list --endpoint 'http://127.0.0.1:4445' --fake-tls-termination
-fi
