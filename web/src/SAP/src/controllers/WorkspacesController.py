1dfe7341ea8e310dad61934ef6aeaad14ec14a5e
Allan Hvam <allanhp@hotmail.com>
2024-06-13 14:58:04 +0200

    feat: start distance matrix job

--- web/src/SAP/src/controllers/WorkspacesController.py
+++ web/src/SAP/src/controllers/WorkspacesController.py
@@ -1,6 +1,11 @@
 import sys
 from flask import abort
 from flask.json import jsonify
+from web.src.services.bio_api.openapi.api.distances_api import DistancesApi
+from web.src.services.bio_api.openapi.api.nearest_neighbors_api import NearestNeighborsApi
+from web.src.services.bio_api.openapi.api_client import ApiClient
+from web.src.services.bio_api.openapi.configuration import Configuration
+from web.src.services.bio_api.openapi.model.distance_matrix_request import DistanceMatrixRequest
 from ..repositories.workspaces import get_workspaces as get_workspaces_db
 from ..repositories.workspaces import delete_workspace as delete_workspace_db
 from ..repositories.workspaces import delete_workspace_sample as delete_workspace_sample_db
@@ -34,3 +39,15 @@ def get_workspace(user, token_info, workspace_id: str):
 def delete_workspace_sample(user, token_info, workspace_id, sample_id):
     res = delete_workspace_sample_db(user, workspace_id, sample_id)
     return None if res.modified_count > 0 else abort(404)
+
+def build_workspace_tree(user, token_info, workspace_id, body):
+    workspace = get_workspace_db(user, workspace_id)
+    samples = list(map(lambda s: s["id"], workspace["samples"]))
+
+    with ApiClient(Configuration(host="http://bio_api:8000")) as api_client:
+        api_instance = DistancesApi(api_client)
+        request = DistanceMatrixRequest("samples", "_id", "categories.cgmlst.report.alleles", samples)
+        api_response = api_instance.dmx_from_mongodb_v1_distance_calculations_post(request)
+        job_id = api_response["job_id"]
+
+    return "ok"
