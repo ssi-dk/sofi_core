c3e28729ac36d95882dab620a524b8ed08c13597
Allan Hvam <allanhp@hotmail.com>
2024-06-21 09:13:00 +0200

    fix: added key check

--- web/src/SAP/src/controllers/MicroreactController.py
+++ web/src/SAP/src/controllers/MicroreactController.py
@@ -18,61 +18,10 @@ def send_to_microreact(user, token_info, body):
     for sample in ws["samples"] :
         row = []
         for column in authorized :
-            ## Dette må kunne gøres smartere ved automatisk at benytte tabellen sap_analysis_results, 
-            ## til at beskrive hvilke rækker som skal medtages/fravælges:
-            if column not in [ 
-                "project_number", 
-                "project_title", 
-                "date_sample", 
-                "date_received", 
-                "public_number",
-                "primary_isolate",
-                "chr_number",
-                "cvr_number",
-                "aut_number",
-                "product_type",
-                "product",
-                "origin_country",
-                "animal_species",
-                "sample_info",
-                "date_received_kma",
-                "region",
-                "fud_number",
-                "cluster_id",
-                "date_epi",
-                "resfinder_version",
-                "date_approved_serotype",
-                "date_approved_qc",
-                "date_approved_amr",
-                "date_approved_st",
-                "date_approved_toxin",
-                "date_approved_cluster",
-                "qc_provided_species",
-                "qc_cgmlst_percent",
-                "species_final",
-                "pathotype",
-                "pathotype_final",
-                "sero_enterobase",
-                "sero_seqsero",
-                "sero_antigen_seqsero",
-                "sero_serotype_finder",
-                "serotype_final",
-                "adhesion_final",
-                "virulence_genes",
-                "toxins_final",
-                "infection_source",
-                "amr_profile",
-                "comment",
-                "comment_cluster",
-                "comment_supplementary",
-                "comment_qc",
-                "cgmlst_schema_salmonella",
-                "cgmlst_schema_ecoli",
-                "cgmlst_schema_campylobacter",
-                "cgmlst_schema_listeria",
-                "cgmlst_schema_klebsiella"
-                ]:
+            if column in sample:
                 row.append(sample[column])
+            else:
+                row.append("")
         values.append(row)
 
     print(values, file=sys.stderr)
