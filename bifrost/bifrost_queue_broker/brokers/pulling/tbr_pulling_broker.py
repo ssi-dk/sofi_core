a6458643f7b94dc7ce2a8a8662a83a3e0ff92d66
Simon J.K. Pedersen (Delegate) <skp@delegate.dk>
2024-09-09 13:47:51 +0200

    fix: logic

--- bifrost/bifrost_queue_broker/brokers/pulling/tbr_pulling_broker.py
+++ bifrost/bifrost_queue_broker/brokers/pulling/tbr_pulling_broker.py
@@ -85,10 +85,12 @@ class TBRPullingBroker(threading.Thread):
             # {"$unwind": {"path": "$metadata", "preserveNullAndEmptyArrays": True}},
             {
                 "$match": {
-                    "$or": [
-                         {"isolate_id": {"$ne": null}},
-                        {"metadata.gdpr_deleted": {"$exists": False}},
-                        {"metadata.gdpr_deleted": False},
+                    "$and": [
+                        {"isolate_id": {"$ne": null}},
+                        {"$or": [                         
+                            {"metadata.gdpr_deleted": {"$exists": False}},
+                            {"metadata.gdpr_deleted": False},
+                        ]}
                     ]
                 }
             },
