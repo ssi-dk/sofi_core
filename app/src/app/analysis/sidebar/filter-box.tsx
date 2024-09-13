167159d076c0cf729ec694e099d8c10964f1165b
Kasper Fuglsang Christensen (Delegate) <kfc@delegate.dk>
2024-09-13 11:11:34 +0200

    feat: add tooltips to sidebar filters

--- app/src/app/analysis/sidebar/filter-box.tsx
+++ app/src/app/analysis/sidebar/filter-box.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, useDisclosure } from "@chakra-ui/react";
+import { QuestionIcon } from "@chakra-ui/icons";
+import FilterHelpModal from "./filter-help-modal";
 
 type FilterBoxProps = {
   title: string;
@@ -7,10 +9,29 @@ type FilterBoxProps = {
 };
 
 function FilterBox({ title, children }: FilterBoxProps) {
+  const {
+    isOpen: isFilterHelpModalOpen,
+    onOpen: onFilterHelpModalOpen,
+    onClose: onFilterHelpModalClose,
+  } = useDisclosure();
   return (
     <Box borderWidth="1px" rounded="md" minH={200} p={2}>
       <Heading as="h5" size="sm" mb={3}>
-        {title}
+        <div style={{ display: "flex" }}>
+        <div>{title}</div>
+        <div style={{ marginLeft: "auto" }}>
+          <React.Fragment >
+              <FilterHelpModal 
+                isOpen={isFilterHelpModalOpen}
+                onClose={onFilterHelpModalClose}
+              />
+              <QuestionIcon
+                    color="gray.400"
+                    onClick={onFilterHelpModalOpen}
+                    cursor="pointer"
+                  />
+            </React.Fragment></div>
+        </div>
       </Heading>
       {children}
     </Box>
---
+++ app/src/app/analysis/sidebar/filter-help-modal.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  Box,
+  Button,
+  Heading,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
+
+type FilterHelpModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+const FilterHelpModal = (props: FilterHelpModalProps) => {
+  const { t } = useTranslation();
+  const { isOpen, onClose } = props;
+
+  return (
+    <React.Fragment>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        size="6xl"
+        scrollBehavior="inside"
+      >
+        <ModalOverlay />
+        <ModalContent mt="0">
+          <ModalHeader pl="7">{t("Filter Help")}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody overflowY="auto" px="7">
+            <div>
+              <Box>
+                Filters can be applied to filter out irrelevant results from the current results.
+              </Box>
+              <br/>
+              <Heading size="s">Note</Heading>
+              <Box>
+                Filters work only on the currently displayed results.
+              </Box>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              {t("Close")}
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </React.Fragment>
+  );
+};
+
+export default React.memo(FilterHelpModal);
