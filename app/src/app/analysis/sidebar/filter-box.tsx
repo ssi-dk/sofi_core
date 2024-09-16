91f3a6ee3e3642320ef418fcda32e751dc7e5555
Kasper Fuglsang Christensen (Delegate) <kfc@delegate.dk>
2024-09-16 14:37:19 +0200

    feat: format, remove unnecessary wrapping elements, change "OBS" to "NB!"

--- app/src/app/analysis/sidebar/filter-box.tsx
+++ app/src/app/analysis/sidebar/filter-box.tsx
@@ -3,12 +3,12 @@ import { Box, Heading, useDisclosure } from "@chakra-ui/react";
 import { QuestionIcon } from "@chakra-ui/icons";
 import FilterHelpModal from "./filter-help-modal";
 
-type FilterBoxProps = {
+type Props = {
   title: string;
   children: JSX.Element | JSX.Element[];
 };
 
-function FilterBox({ title, children }: FilterBoxProps) {
+function FilterBox({ title, children }: Props) {
   const {
     isOpen: isFilterHelpModalOpen,
     onOpen: onFilterHelpModalOpen,
@@ -18,19 +18,18 @@ function FilterBox({ title, children }: FilterBoxProps) {
     <Box borderWidth="1px" rounded="md" minH={200} p={2}>
       <Heading as="h5" size="sm" mb={3}>
         <div style={{ display: "flex" }}>
-        <div>{title}</div>
-        <div style={{ marginLeft: "auto" }}>
-          <React.Fragment >
-              <FilterHelpModal 
-                isOpen={isFilterHelpModalOpen}
-                onClose={onFilterHelpModalClose}
-              />
-              <QuestionIcon
-                    color="gray.400"
-                    onClick={onFilterHelpModalOpen}
-                    cursor="pointer"
-                  />
-            </React.Fragment></div>
+          <div>{title}</div>
+          <div style={{ marginLeft: "auto" }}>
+            <FilterHelpModal
+              isOpen={isFilterHelpModalOpen}
+              onClose={onFilterHelpModalClose}
+            />
+            <QuestionIcon
+              color="gray.400"
+              onClick={onFilterHelpModalOpen}
+              cursor="pointer"
+            />
+          </div>
         </div>
       </Heading>
       {children}
--- app/src/app/analysis/sidebar/filter-help-modal.tsx
+++ app/src/app/analysis/sidebar/filter-help-modal.tsx
@@ -13,47 +13,43 @@ import {
 } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 
-type FilterHelpModalProps = {
+type Props = {
   isOpen: boolean;
   onClose: () => void;
 };
 
-const FilterHelpModal = (props: FilterHelpModalProps) => {
+const FilterHelpModal = (props: Props) => {
   const { t } = useTranslation();
   const { isOpen, onClose } = props;
 
   return (
-    <React.Fragment>
-      <Modal
-        isOpen={isOpen}
-        onClose={onClose}
-        size="6xl"
-        scrollBehavior="inside"
-      >
-        <ModalOverlay />
-        <ModalContent mt="0">
-          <ModalHeader pl="7">{t("Filter Help")}</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody overflowY="auto" px="7">
-            <div>
-              <Box>
-                Filters can be applied to filter out irrelevant results from the current results.
-              </Box>
-              <br/>
-              <Heading size="s">OBS</Heading>
-              <Box>
-                Filters only work on the currently displayed results.
-              </Box>
-            </div>
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              {t("Close")}
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </React.Fragment>
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      size="6xl"
+      scrollBehavior="inside"
+    >
+      <ModalOverlay />
+      <ModalContent mt="0">
+        <ModalHeader pl="7">{t("Filter Help")}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody overflowY="auto" px="7">
+          <Box>
+            Filters can be applied to filter out irrelevant results from the current results.
+          </Box>
+          <br />
+          <Heading size="s">NB!</Heading>
+          <Box>
+            Filters only work on the currently displayed results.
+          </Box>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onClose}>
+            {t("Close")}
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   );
 };
 
