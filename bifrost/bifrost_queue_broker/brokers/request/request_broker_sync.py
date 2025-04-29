84d391ab96aa11c6cfba3ea6106432b003bff85f
Kasper Fuglsang Christensen (Delegate) <kfc@delegate.dk>
2025-04-29 13:26:45 +0200

    feat: change request broker cursor to non-tailable

--- bifrost/bifrost_queue_broker/brokers/request/request_broker_sync.py
+++ bifrost/bifrost_queue_broker/brokers/request/request_broker_sync.py
@@ -1,6 +1,7 @@
 import logging
 from pymongo import CursorType
 from ..shared import ProcessingStatus
+from time import sleep
 
 
 class RequestBrokerSync():
@@ -11,26 +12,30 @@ class RequestBrokerSync():
         self.broker_name = name
         self.listener_match = matcher
         self.request_callback = callback
+        self.sleep_time = 10
 
     def run(self):
         logging.info(f"Started {self.broker_name} thread.")
-        cursor = self.col.find(
-            self.listener_match, cursor_type=CursorType.TAILABLE_AWAIT
-        )
-        while cursor.hasNext():
-                record = cursor.next()
-                logging.info(f"{self.broker_name} trying to DB lock {record['_id']}")
-                # We cannot change size of documents in capped collections
-                # so the "locking" should be the same length in both get and update.
-                self.col.update(
-                    {
-                        "_id": record["_id"],
-                        "status": ProcessingStatus.WAITING.value,
-                    },
-                    {"$set": {"status": ProcessingStatus.PROCESSING.value}},
-                )
-                self.request_callback(record)
-                self.mark_done(record)
+        while True:
+            logging.info(f"{self.broker_name} listening for requests.")
+            cursor = self.col.find(
+                self.listener_match, cursor_type=CursorType.NON_TAILABLE
+            )
+            for record in cursor:
+                    logging.info(f"{self.broker_name} trying to DB lock {record['_id']}")
+                    # We cannot change size of documents in capped collections
+                    # so the "locking" should be the same length in both get and update.
+                    self.col.update(
+                        {
+                            "_id": record["_id"],
+                            "status": ProcessingStatus.WAITING.value,
+                        },
+                        {"$set": {"status": ProcessingStatus.PROCESSING.value}},
+                    )
+                    self.request_callback(record)
+                    self.mark_done(record)
+            logging.info(f"{self.broker_name} finished processing requests. Sleeping for {self.sleep_time}s.")
+            sleep(self.sleep_time)
 
     def mark_done(self, record):
         record["status"] = ProcessingStatus.DONE.value
