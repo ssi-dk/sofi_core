0aa97b30144970b9d86f65688ed96abf546a78c8
Thor Frøding Skipper <tfs@contextand.com>
2025-10-09 13:04:58 +0200

    chore: removed logs

--- app/src/app/analysis/sidebar/meta-filter/meta-filter.tsx
+++ app/src/app/analysis/sidebar/meta-filter/meta-filter.tsx
@@ -75,7 +75,6 @@ function MetaFilter(props: MetaFilterProps) {
     ) => (d: Date) => {
       cb(d);
 
-      console.log(field,end,"changed. Old state:",rangeFilterState)
 
       const opposite = end === "min" ? "max" : "min";
       const minDate = null;
@@ -93,7 +92,6 @@ function MetaFilter(props: MetaFilterProps) {
       if (val == null && oppositeValue == null) {
         clearFieldFromSearch(field);
       }
-      console.log("resolved state:",resolvedState)
 
       setRangeFilterState(resolvedState);
       onRangeFilterChange(resolvedState);
