085aba298cb0f31f19bb9528ead49601f6e4ef76
Kasper Fuglsang Christensen (Delegate) <kfc@delegate.dk>
2025-10-22 10:54:18 +0200

    Revert "feat: history pinning"

--- app/src/app/analysis/search/search-history.tsx
+++ app/src/app/analysis/search/search-history.tsx
@@ -1,11 +1,11 @@
 import { Button, Flex, IconButton } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { deRegisterHistoryCB, displayOperandName, getSearchHistory, recurseSearchTree, registerHistoryCB, SearchHistory, setPinned } from "./search-utils";
-import { SearchIcon, StarIcon } from "@chakra-ui/icons";
-import { AnalysisQuery } from "sap-client";
+import { deRegisterHistoryCB, displayOperandName, getSearchHistory, recurseSearchTree, registerHistoryCB, SearchHistory } from "./search-utils";
+import { SearchIcon } from "@chakra-ui/icons";
+import { QueryExpression } from "sap-client";
 
 const SearchHistoryMenu = (props: {
-    onSearchChange: (query: AnalysisQuery) => void
+    onSearchChange: (query: QueryExpression) => void
 }) => {
     const { onSearchChange } = props;
     const [searchHistory, setSearchHistory] = useState<SearchHistory>([]);
@@ -22,13 +22,6 @@ const SearchHistoryMenu = (props: {
         <Flex direction="column" style={{ marginLeft: "6px" }}>
             {/* TODO: Make pretty */}
             {searchHistory.map(s => <Flex direction="row" alignItems="center" key={s.timestamp} style={{margin: "4px",background: "lightgray", borderRadius: "4px"}} >
-                <IconButton
-                    aria-label="Pin"
-                    icon={<StarIcon {...(s.pinned ? { color: "gold" } : { fillOpacity: 0, stroke: "black" })} />}
-                    ml="1"
-                    onClick={() => setPinned(s,!s.pinned)}
-                    style={{marginRight: "4px"}}
-                />
                 <IconButton
                     aria-label="Search database"
                     icon={<SearchIcon />}
