55f185a36b4cdce3ef2784616b3a99c0b118c6a3
Thor Frøding Skipper <tfs@contextand.com>
2026-01-29 10:47:55 +0100

    fix: hide old workspaces

--- app/src/app/header/nav-bar.tsx
+++ app/src/app/header/nav-bar.tsx
@@ -20,9 +20,9 @@ function NavBar() {
       <NavLink to="/">
         <Button leftIcon={<EditIcon />}>{t("Analysis results")}</Button>
       </NavLink>
-      <NavLink to="/workspaces">
+      {/* <NavLink to="/workspaces">
         <Button leftIcon={<ViewIcon />}>{t("Workspaces")}</Button>
-      </NavLink>
+      </NavLink> */}
       <NavLink to="/clusters">
         <Button leftIcon={<HamburgerIcon />}>{t("Clusters")}</Button>
       </NavLink>
