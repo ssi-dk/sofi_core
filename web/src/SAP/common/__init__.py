4e6b6a2da40bb2360130fcdc5a374b5252b76bc2
Thor Frøding Skipper <tfs@contextand.com>
2026-04-08 12:36:01 +0200

    chore: migration explanation

--- web/src/SAP/common/__init__.py
+++ web/src/SAP/common/__init__.py
@@ -7,15 +7,18 @@ conn = get_connection()
 mydb = conn[DB_NAME]
 migrations_coll = mydb[MIGRATIONS_COL_NAME]
 
-already_executed = list(map(lambda x: x["name"], migrations_coll.find()))
 
+# The migrations work my dynamically loading all the functions in the migrations.py file. Then it compares these function names to the ones in the migrations collection
+#  in the database. All migration functions whoose names are not in the migrations collection are executed, and after they are executed they are added to the collection.
+#  This way all migrations are executed exactly once on each database.
+
+already_executed = list(map(lambda x: x["name"], migrations_coll.find()))
 
 migration_funcs = [
     func for name, func in inspect.getmembers(migrations, inspect.isfunction)
     if func.__module__ == migrations.__name__  # only functions defined there
 ]
 
-# Example: conditionally execute
 for func in migration_funcs:
     name = func.__name__ 
 
